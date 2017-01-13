af1298e6a0b0f60cc6f6188c38355676c649759f
SebastianMunozED <sebastian.munoz1146@gmail.com>
2017-01-13 00:14:11 -0500

    Update SlinkedList.java

--- SlinkedList.java
+++ SlinkedList.java
@@ -11,49 +11,49 @@ public class SlinkedList {
 	
 	public void InsertarAlFinal(Nodo s){
 		if(head == null){
-			head= s;
-			size=1;
+			head = s;
+			size = 1;
 		}
 		else{
 			Nodo aux;
-			aux= head;
-			while(aux.getEnlace()!=null){
+			aux = head;
+			while(aux.getEnlace() != null){
 				aux=aux.getEnlace();
 			}
 			aux.setEnlace(s);
-			size=size+1;
+			size = size + 1;
 		}
 	}
 	
 	public void InsertarAlInicio(Nodo s){
-		if(head==null){
-			head= s;
-			size=1;
+		if(head == null){
+			head = s;
+			size = 1;
 			}
 		else{
 			Nodo aux=head;
 			s.setEnlace(aux);
 			head = s;
-			size = size+1;
+			size = size + 1;
 		}	
 	}
 	public boolean buscar(String dato){
-        Nodo aux = head;
-        boolean existe = false;
-        //Recorre la lista 
-        while(aux != null && existe != true){
-            if (dato == aux.getDato()){
-                existe = true;
-            }
-            else{
-                //Siguiente nodo
-                aux = aux.getEnlace();
-            }
-        }
-        return existe;
-    }
+		Nodo aux = head;
+		boolean existe = false;
+		//Recorre la lista 
+		while(aux != null && existe != true){
+		    if (dato == aux.getDato()){
+			existe = true;
+		    }
+		    else{
+			//Siguiente nodo
+			aux = aux.getEnlace();
+		    }
+		} return existe;
+	    }
 
 	public int getSize(){
+		//Tamaño
 	        return size;
 	    }
 	
