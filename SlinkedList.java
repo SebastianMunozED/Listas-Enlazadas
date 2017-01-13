3a3cd83bb619742d4a58ef5cdb93b3ee35be24c3
SebastianMunozED <sebastian.munoz1146@gmail.com>
2017-01-13 12:15:02 -0500

    Update SlinkedList.java

--- SlinkedList.java
+++ SlinkedList.java
@@ -1,4 +1,7 @@
 package epn.com;
+
+import java.io.IOException;
+
 public class SlinkedList {
 
 	protected Nodo head;
@@ -9,6 +12,26 @@ public class SlinkedList {
 		size = 0;		
 	}
 	
+	public boolean esVacia(){
+        return head == null;
+    }
+	
+	 public int getPosicion(String referencia) throws Exception{
+	        if (buscar(referencia)) {
+	            Nodo aux = head;
+	            int cont = 1;
+	            while(referencia != aux.getDato()){
+	                cont ++;
+	                aux = aux.getEnlace();
+	            }
+	            return cont;
+	        
+	        } else {
+	            throw new Exception("Valor inexistente en la lista.");
+	        }
+	    }
+	
+	
 	public void InsertarAlFinal(Nodo s){
 		if(head == null){
 			head = s;
@@ -16,8 +39,8 @@ public class SlinkedList {
 		}
 		else{
 			Nodo aux;
-			aux = head;
-			while(aux.getEnlace() != null){
+			aux= head;
+			while(aux.getEnlace()!=null){
 				aux=aux.getEnlace();
 			}
 			aux.setEnlace(s);
@@ -26,44 +49,43 @@ public class SlinkedList {
 	}
 	
 	public void InsertarAlInicio(Nodo s){
-		if(head == null){
-			head = s;
-			size = 1;
+		if(head==null){
+			head= s;
+			size=1;
 			}
 		else{
 			Nodo aux=head;
 			s.setEnlace(aux);
 			head = s;
-			size = size + 1;
+			size = size+1;
 		}	
 	}
 	public boolean buscar(String dato){
-		Nodo aux = head;
-		boolean existe = false;
-		//Recorre la lista 
-		while(aux != null && existe != true){
-		    if (dato == aux.getDato()){
-			existe = true;
-		    }
-		    else{
-			//Siguiente nodo
-			aux = aux.getEnlace();
-		    }
-		} return existe;
-	    }
+        Nodo aux = head;
+        boolean existe = false;
+        //Recorre la lista 
+        while(aux != null && existe != true){
+            if (dato == aux.getDato()){
+                existe = true;
+            }
+            else{
+                aux = aux.getEnlace();
+            }
+        }
+        return existe;
+    }
 
 	public int getSize(){
-		//Tamaño
 	        return size;
 	    }
 	
 	
 	@Override
 	public String toString() {
-		String lista="";
-		Nodo aux=head;
+		String lista = "";
+		Nodo aux = head;
 		lista += aux.getDato()+"\n";
-		while(aux.getEnlace()!= null){
+		while(aux.getEnlace() != null){
 			lista += aux.getEnlace().getDato()+"\n";
 			aux = aux.getEnlace();
 		}
